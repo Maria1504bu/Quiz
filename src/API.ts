b607307f8b36313184b6679c147a2b9f82d0d134
Maria1504bu <15504420011m@gmail.com>
2023-01-09 15:44:43 +0200

    change fetch url

--- src/API.ts
+++ src/API.ts
@@ -19,7 +19,7 @@ export enum Difficulty {
 }
 
 export const fetchQuizQuestions = async (amount :number, difficulty : Difficulty) => {
-    const endpoint = `http://opentdb.com/api.php?amount=${amount}&category=12&difficulty=${difficulty}&type=multiple`
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=12&difficulty=${difficulty}&type=multiple`
     const data = await (await fetch(endpoint)).json();
     return data.results.map((question : Question) => ({
         ...question,
